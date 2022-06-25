bfb45107e7f757998412c36033f85122690a2d31
James Hill <james@onehilltech.com>
2022-06-24 20:02:39 -0400

    fix: removed include() override

--- index.js
+++ index.js
@@ -1,18 +1,5 @@
 'use strict';
 
 module.exports = {
-  name: require('./package').name,
-
-  contentFor (type, config) {
-    this._super (...arguments);
-
-    const blueprintConfig = config['ember-blueprint-app'] || {};
-    const { enabled = !!process.env.npm_package_name } = blueprintConfig;
-
-    if (enabled) {
-      if (type === 'head') {
-        return `{{>blueprint.head}}`;
-      }
-    }
-  }
+  name: require('./package').name
 };
